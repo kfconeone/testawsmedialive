d58af2474928d1924f8a5255ad4b242eb8b0031f
kfconeone <kfconeone@gmail.com>
2022-12-14 13:18:00 +0800

    init

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <video-js ref="videoPlayer" class="video-js" controls>
+      <source :src="hlsUrl" type="application/x-mpegURL" />
+    </video-js>
+  </div>
+</template>
+
+<script>
+import videojs from 'video.js'
+
+export default {
+  data() {
+    return {
+      hlsUrl: "http://cx7nx2tbdgudme.data.mediastore.us-east-1.amazonaws.com/livea/main.m3u8"
+    }
+  },
+  mounted() {
+    this.player = videojs(this.$refs.videoPlayer, {
+      // options
+    })
+  }
+}
+</script>
