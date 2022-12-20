f1289236855c26d096a98c3b0a7caa7e474cf4f9
kfconeone <kfconeone@gmail.com>
2022-12-20 12:15:22 +0800

    change to webrtc

--- app.vue
+++ app.vue
@@ -1,24 +1,37 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import videojs, { VideoJsPlayer } from 'video.js'
+// import { WowzaWebRTCPlayer } from 'wowza-webrtc-player';
+
+
+
+const videoPlayer = ref();
+var player: VideoJsPlayer;
+
+onMounted(async () => {
+  if (process.client) {
+    let wowza = await import("wowza-webrtc-player");
+    const player = new wowza.WowzaWebRTCPlayer(videoPlayer.value, {
+      sdpUrl: 'wss://639fdc2445489.streamlock.net:1955/webrtc-session.json',
+      applicationName: 'testLive',
+      streamName: 'rtck',
+    });
+
+    await player.playRemote();
+    console.log(player)
+  }
+
+});
+
+</script>
 <template>
+
+  <Head>
+    <Link rel="stylesheet" href="https://unpkg.com/video.js/dist/video-js.css" />
+  </Head>
   <div>
-    <video-js ref="videoPlayer" class="video-js" controls>
-      <source :src="hlsUrl" type="application/x-mpegURL" />
-    </video-js>
+    <div>
+      <video ref="videoPlayer" autoplay muted style="width: 100vw; height: 56.25vw"></video>
+    </div>
   </div>
 </template>
-
-<script>
-import videojs from 'video.js'
-
-export default {
-  data() {
-    return {
-      hlsUrl: "https://cx7nx2tbdgudme.data.mediastore.us-east-1.amazonaws.com/livea/main.m3u8"
-    }
-  },
-  mounted() {
-    this.player = videojs(this.$refs.videoPlayer, {
-      // options
-    })
-  }
-}
-</script>
